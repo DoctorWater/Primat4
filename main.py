c6d951f7a1db6b7d1e7668be6e936add1a6dd8f4
DoctorWater <malckov.schura2015@gmail.com>
2023-06-01 04:22:10 +0300

    5 corrected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import scipy.sparse as sp
-import numpy.linalg as la
 from matplotlib import pyplot as plt
 
 
@@ -8,6 +7,12 @@ def generate_system_data(n, k):
     A = generate_matrix_A(n, k)
     F = generate_vector_F(n)
     return A, F
+
+
+def compute_error(A, x, b):
+    Ax = np.dot(A, x)
+    error = np.linalg.norm(Ax - b)
+    return error
 
 
 def generate_matrix_A(n, k):
@@ -19,11 +24,11 @@ def generate_matrix_A(n, k):
             if i != j:
                 A[i, j] = np.random.choice([-4, -3, -2, -1, 0])
                 row_sum += abs(A[i, j])
-
-        if i == 0:
-            A[i, i] = -row_sum + 10 ** (-k)
-        else:
-            A[i, i] = -row_sum
+            else:
+                if i > 0:
+                    A[i, i] = -row_sum
+                else:
+                    A[i, i] = -row_sum + 10.0 ** (-k)
 
     return A
 
@@ -51,14 +56,15 @@ def gauss_elimination(A, b):
 
         # Приведение к треугольному виду
         for j in range(i + 1, n):
-            factor = A[j, i] // A[i, i]
+            factor = A[j, i] / A[i, i]
             A[j, i:] -= factor * A[i, i:]
             b[j] -= factor * b[i]
 
     # Обратный ход метода Гаусса
     x = np.zeros(n, dtype=float)
     for i in range(n - 1, -1, -1):
-        x[i] = (b[i] - np.dot(A[i, i + 1:], x[i + 1:])) // A[i, i]
+        if A[i, i] != 0:
+            x[i] = (b[i] - np.dot(A[i, i + 1:], x[i + 1:])) / A[i, i]
 
     return x
 
@@ -66,18 +72,22 @@ def gauss_elimination(A, b):
 # Метод LU-разложения
 def lu_decomposition(A):
     n = A.shape[0]
-    L = sp.eye(n, format='lil')  # Единичная матрица L
-    U = sp.lil_matrix((n, n))  # Пустая матрица U
+    L = sp.eye(n, format='csr')  # Единичная матрица L
+    U = sp.csr_matrix((n, n))  # Пустая матрица U
 
     for k in range(n):
         U[k, k] = A[k, k] - L[k, :k].dot(U[:k, k].toarray().flatten())
+
+        if U[k, k] == 0:
+            L[k, k:] = 0
+            continue
+
         for i in range(k + 1, n):
             L[i, k] = (A[i, k] - L[i, :k].dot(U[:k, k].toarray().flatten())) / U[k, k]
         for j in range(k + 1, n):
             U[k, j] = A[k, j] - L[k, :k].dot(U[:k, j].toarray().flatten())
 
     return L, U
-
 
 def solve_lu(L, U, b):
     n = L.shape[0]
@@ -86,11 +96,17 @@ def solve_lu(L, U, b):
 
     # Решение Ly = b
     for i in range(n):
-        y[i] = (b[i] - L[i, :i].dot(y[:i])) / L[i, i]
+        if L[i, i] != 0:
+            y[i] = (b[i] - L[i, :i].dot(y[:i])) / L[i, i]
+        else:
+            y[i] = 0
 
     # Решение Ux = y
     for i in range(n - 1, -1, -1):
-        x[i] = (y[i] - U[i, i + 1:].dot(x[i + 1:])) / U[i, i]
+        if U[i, i] != 0:
+            x[i] = (y[i] - U[i, i + 1:].dot(x[i + 1:])) / U[i, i]
+        else:
+            x[i] = 0
 
     return x
 
@@ -103,7 +119,8 @@ def jacobi(A, b, x0, max_iterations=100, tolerance=1e-6):
     for _ in range(max_iterations):
         x_new = np.zeros(n)
         for i in range(n):
-            x_new[i] = (b[i] - A[i, :].dot(x) + A[i, i] * x[i]) / A[i, i]
+            if A[i, i] != 0:
+                x_new[i] = (b[i] - A[i, :].dot(x) + A[i, i] * x[i]) / A[i, i]
 
         if np.linalg.norm(x_new - x) < tolerance:
             return x_new
@@ -113,40 +130,60 @@ def jacobi(A, b, x0, max_iterations=100, tolerance=1e-6):
     return x
 
 
-def evaluate_jacobi(N, K):
+n = 3  # Размерность СЛАУ
+k = np.arange(1, 10)  # Значение k для определения числа обусловленности
+
+# Вызов функций для решения СЛАУ с матрицей A и вектором F
+
+error_gauss = []
+error_lu = []
+error_jacobi = []
+
+for k_cur in k:
     while 1:
-        A, F = generate_system_data(N, K)
+        A, F = generate_system_data(n, k_cur)
         det_A = np.linalg.det(A)
         if det_A != 0:
             break
+
     x0 = np.zeros(n)
+    L, U = lu_decomposition(A)
+    x = gauss_elimination(A, F)
+    error_gauss.append(compute_error(A, x, F))
+
+    x = solve_lu(L, U, F)
+    error_lu.append(compute_error(A, x, F))
+
     x = jacobi(A, F, x0)
-    cond1 = la.cond(A)
-    exact_solution = np.linalg.solve(A, F)
-    error1 = la.norm(x - exact_solution)
-    return cond1, error1
+    error_jacobi.append(compute_error(A, x, F))
 
+# Построение графика
+plt.plot(k, error_gauss)
 
-n = 3
-k_values = [i for i in range(1, 10)]
-cond_values = []
-error_values = []
+# Настройка осей и заголовка
+plt.xlabel('Коэффициент k')
+plt.ylabel('Погрешность')
+plt.title('Погрешности при изменении k (Метод Гаусса)')
 
-for k in k_values:
-    cond, error = evaluate_jacobi(n, k)
-    cond_values.append(cond)
-    error_values.append(error)
-
-plt.plot(k_values, cond_values, label="Number of Condition")
-plt.xlabel('k')
-plt.ylabel('Condition Number')
-plt.title('Dependency of Condition Number on k')
-plt.legend()
+# Отображение графика
 plt.show()
 
-plt.plot(k_values, error_values, label="Solution Error")
-plt.xlabel('k')
-plt.ylabel('Error')
-plt.title('Dependency of Solution Error on k')
-plt.legend()
+plt.plot(k, error_lu)
+
+# Настройка осей и заголовка
+plt.xlabel('Коэффициент k')
+plt.ylabel('Погрешность')
+plt.title('Погрешности при изменении k (LU)')
+
+# Отображение графика
+plt.show()
+
+plt.plot(k, error_jacobi)
+
+# Настройка осей и заголовка
+plt.xlabel('Коэффициент k')
+plt.ylabel('Погрешность')
+plt.title('Погрешности при изменении k (Метод Якоби)')
+
+# Отображение графика
 plt.show()
